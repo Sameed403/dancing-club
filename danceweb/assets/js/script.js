e74a0feb12b8dd3da1bff4a324b941a0fe576ac7
Sameed <sameedtariq70@gmail.com>
2022-10-10 22:24:47 +0500

    Initial Commit

---
+++ danceweb/assets/js/script.js
@@ -0,0 +1,7 @@
+let menu_button = document.getElementById('menu_icon');
+let mobile_nav = document.getElementById('mobile_nav');
+
+menu_button.addEventListener('click' , () => {
+    mobile_nav.classList.toggle('action');;
+   console.log('sameed');
+})
